91430bb476e05f5715a7beb0d73c8aeca45aac21
amyrria <abdami.4624@gmail.com>
2018-11-18 02:49:33 -0400

    Removida funçao BFS e DFS

--- BFS-DFS.cpp
+++ BFS-DFS.cpp
@@ -1,13 +1,13 @@
 #include <cstdlib>
 #include <iostream>
-#define inf -1
+#define INFINITO -1
 using namespace std;
 
 
 template <class T>
 class Node {
 public:
-    Node(const T &e = T(),const double&w = 0, Node *n = NULL, Node *p = NULL) : elemento(e), peso(w), prox(n), anterior(p) {}
+    Node(const T &elemento = T(),const double&custo = 0, Node *n = NULL, Node *p = NULL) : elemento(elemento), peso(custo), prox(n), anterior(p) {}
     ~Node() {}
     int get_Element(){return elemento;};
     int get_Peso(){return peso;};
@@ -20,52 +20,57 @@ public:
 
 template <class T> class NodeFila 
 {
-private:
-    T Item;
-    NodeFila *prox;
 public:
-    NodeFila(){}
-    T getItem(){return this->Item;}
-    NodeFila *getProx(){return this->prox;}
-    void setItem(T Item){this->Item = Item;}
-    void setProx(NodeFila *prox){this->prox = prox;}
+    T item;
+    double custo;
+    NodeFila *prox;
+    NodeFila(const T &item = T(),const double&custo = 0, NodeFila *n = NULL) : item(item), custo(custo), prox(n){}
+    ~NodeFila(){}
+    //void setItem(T Item){this->Item = Item;}
+    T getItem(){return this->item;}
+    NodeFila *getProx(){return this->prox;}    
+    //void setProx(NodeFila *prox){this->prox = prox;}
 };
 
 template <class T> class Fila 
 {
 public:
-    int size;
-    NodeFila<T> *front, *back;
+    int tamanho;
+    NodeFila<T> *frente, *atras;
 public:
     Fila();
-    void enfileirar(T&);
+    void enfileirar(T &item, double custo);
     T desenfileirar();
     bool empty();
+    ~Fila() {
+    delete frente;
+    delete atras;
+}
 };
+
 
 template <class T> Fila<T>::Fila() 
 {
-    back = new NodeFila<T>();
-    back->setProx(NULL);
-    front = back;
-    size = 0;
+    frente = new NodeFila<T>();
+    atras = new NodeFila<T>();
+    tamanho = 0;
+
 }
-template <class T> void Fila<T>::enfileirar(T& N) 
+template <class T> void Fila<T>::enfileirar(T& item, double custo) 
 {
-    NodeFila<T> *qNode = new NodeFila<T>();
-    qNode->setProx(NULL);
-    qNode->setItem(N);
-    back->setProx(qNode);
-    back = qNode;
-    if (size==0) front = back;
-    size++;
+    NodeFila<T> *qNode = new NodeFila<T>(item,custo);
+    atras->prox = qNode;
+    atras = qNode;
+    if (tamanho==0) frente = atras;
+    tamanho++;
 }
+
 template <class T> T Fila<T>::desenfileirar()
 {
     if(!empty()){
-        NodeFila<T> *aux = front;
-        front = aux->getProx();
-        size--;
+        NodeFila<T> *aux = frente;
+        frente = aux->getProx();
+        tamanho--;
         T item = aux->getItem();
         delete aux;
         return item;
@@ -74,7 +79,7 @@ template <class T> T Fila<T>::desenfileirar()
     }
 
 }
-template <class T> bool Fila<T>::empty(){return (this->size == 0);}
+template <class T> bool Fila<T>::empty(){return (this->tamanho == 0);}
 
 
 template <class T>
@@ -83,7 +88,7 @@ private:
     Node<T> *cabeca;
     Node<T> *cauda;
     Node<T> *node;
-    int size;
+    int tamanho;
     
 public:
     Lista() {
@@ -94,24 +99,24 @@ public:
         delete cabeca;
         delete cauda;
     }
-    int get_Size(){return this->size;}
+    int get_tamanho(){return this->tamanho;}
     Node<T> * getCabecaProx(){return this->cabeca->prox;}
     Node<T> * getCaudaAnterior(){return this->cauda->anterior;}
     bool isEmpty() { return (cabeca->prox == NULL); }
-    void inserir(T &e, double w) {
+    void inserir(T &elemento, double custo) {
         
         if (!isEmpty()) {
             Node<T> *ultNode = cauda->anterior;
-            Node<T> *newNode = new Node<T>(e,w, NULL, ultNode);
-            ultNode->prox = newNode;
-            cauda->anterior = newNode;
-            size++;
+            Node<T> *novoNode = new Node<T>(elemento,custo, NULL, ultNode);
+            ultNode->prox = novoNode;
+            cauda->anterior = novoNode;
+            tamanho++;
         }
         else {
-            Node<T> *newNode = new Node<T>(e,w);
-            cabeca->prox = newNode;
-            cauda->anterior = newNode;
-            size++;
+            Node<T> *novoNode = new Node<T>(elemento,custo);
+            cabeca->prox = novoNode;
+            cauda->anterior = novoNode;
+            tamanho++;
         }   
     }    
 };
@@ -146,108 +151,42 @@ public:
             };
         };
     }
-    int * BFS(int s, int *inimigos, int e) 
-    { 
-        Fila<int> fila; 
-        int i; 
-        bool *visitado = new bool[V+1]; 
-        int *predecessor = new int[V+1];
-
-        for(int i = 1; i < V+1; i++) visitado[i] = false;
-        for(int i = 0; i<e; i++) {visitado[inimigos[i]] = true; predecessor[inimigos[i]] = -1;}
-
-        visitado[s] = true; 
-        predecessor[s] = -1;
-        fila.enfileirar(s);
-
-      
-        while(!fila.empty()) 
-        { 
-            s = fila.desenfileirar();
-            Node<int> *node;
-            node = adj[s].getCabecaProx();
-
-            while(node!=NULL){
-                i =  node->get_Element();
-                
-                if (!visitado[i]) 
-                {
-                    predecessor[i] = s;
-                    visitado[i] = true; 
-                    fila.enfileirar(i);
-                } 
-                node = node->prox;
-            };
-
-        } 
-        return predecessor;
-        
-    };
-    double DFS_visita(int v, bool visitado[]) 
-    { 
-        int i;
-        visitado[v] = true; 
-        double peso = 0;
-        Node<int> *node;
-        node = adj[v].getCabecaProx();
-        while(node!=NULL){
-            i =  node->get_Element();
-            if (!visitado[i]) 
-            {
-                peso += node->get_Peso();
-                peso += DFS_visita(i, visitado);
-            }
-            node = node->prox;
-        };
-        return peso;
-    };
-    double DFS()
-    { 
-        bool *visitado = new bool[V+1];
-        double peso=0;
-        for (int i = 1; i < V; i++) visitado[i] = false;
-        for (int i = 1; i < V; i++){
-            if(!visitado[i]){
-                peso += DFS_visita(i, visitado); 
-            };
-        };
-        return peso;
-    };
-
   
 };
 int main(){
-    int N, M, u, v,n,m,e, qtdI, orig, dest;
-    double w;
+    int N, M, vertice1, vertice2,n,m,doentes, qtDoentes, orig, dest;
+    double custo;
     cin>> N >> M;
     Grafo G(N); 
     for(int i=1; i<= M;i++) {
-        cin >> u>>v;
-        G.addAresta(u, v, 1);
+        cin >> vertice1 >> vertice2 >> custo;
+        G.addAresta(vertice1, vertice2, custo);
     };
 
-    cin>>qtdI;
-    int *inimigos = new int[qtdI];
-    for(int i=0; i<qtdI;i++){
-        cin >> e;
-        inimigos[i] = e;  
-    }
+    cin>>qtDoentes;
+    int *NeuroniosDoentes = new int[doentes];
+    for(int i=0; i<qtDoentes;i++){
+        cin >> qtDoentes;
+        NeuroniosDoentes[i] = doentes;  
+    };
     cin>> orig >> dest;
-    int *predecessor = G.BFS(orig,inimigos, qtdI);
+    //int *percurso = G.dijkstra(orig, dest);
+    
+
     Grafo *g = new Grafo[N+1];
     for(int i = 1; i <= N; i++){
         cin >> n >> m;
         g[i].iniciar(n);        
         for(int j=0; j < m; j++) {
-            cin >> u >> v >> w;
-            g[i].addAresta(u, v, w);
+            cin >> vertice1 >> vertice2 >> custo;
+            g[i].addAresta(vertice1, vertice2, custo);
         };
 
     };
     double peso=0;
     while(dest != -1){
-        peso += g[dest].DFS();    
-        dest = predecessor[dest];
+        //peso += g[dest].DFS();    
+        //dest = predecessor[dest];
     }    
     cout.precision(1);
     cout << fixed<< peso << endl;
