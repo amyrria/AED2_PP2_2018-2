a58c468bee6ff644bc4e374dea7bb0b8939def40
amyrria <31972902+amyrria@users.noreply.github.com>
2018-11-21 14:30:02 -0400

    Adicionado arquivo dijkstra.h
    Ainda falta concluir

---
+++ dijkstra.h
@@ -0,0 +1,20 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+#include <iostream>
+#include<iomanip>
+using namespace std;
+template <class T>
+class Dijkstra
+{ 
+    private:
+    public:
+	void menorCaminho(int graph[V][V], int src){
+        float distancia[];
+        bool visitados[];
+        
+
+    }
+};
+
+
+#endif
