48f32472c96be2e5afea3d4b3bb6d90d6a498015
Eduardo Morais <emorais1375@gmail.com>
2018-11-25 13:18:29 -0400

    alterações

--- pp2.cpp
+++ pp2.cpp
@@ -5,168 +5,193 @@
 
 using namespace std;
 
-template <class T> class NodeFila{
-public:
-    T item;
-    double dist;
-    NodeFila *prox;
-    NodeFila *ant;
-    NodeFila(const T &item = T(), NodeFila *proximo = NULL, NodeFila *anterior = NULL) : item(item), prox(proximo), ant(anterior){}
-    T getItem(){return item;}
-    double getDist(){return dist;}
-    NodeFila *getProx(){return prox;}
-    NodeFila *getAnt(){return ant;}
-    void setDist(double dist) {this->dist = dist;}
+class Vertice{
+    private:
+        int id;
+        float distancia;
+        Vertice* antecessor;
+    public:
+        Vertice(){}
+        int getId(){
+            return id;
+        }
+        float getDistancia(){
+            return distancia;
+        }
+        Vertice* getAntecessor(){
+            return antecessor;
+        }
+        void setId(int id){
+            this->id = id;
+        }
+        void setDistancia(float d){
+            distancia = d;
+        }
+        void setAntecessor(Vertice* v){
+            antecessor = v;
+        }
+};
+
+template <class T> class No{
+    private:
+        T item;
+        No *prox;
+        No *ant;
+    public:
+        No(const T &item = T(), No *anterior = NULL, No *proximo = NULL) : item(item), ant(anterior), prox(proximo){}
+        T getItem(){return item;}
+        No *getProx(){return prox;}
+        No *getAnt(){return ant;}
+        void setProx(No *p){
+            prox = p;
+        }
+        void setAnt(No *a){
+            ant = a;
+        }
 };
 
 template <class T> class Fila{
-private:
-    int tamanho;
-    NodeFila<T> *frente, *atras;
-public:
-    Fila(){
-        frente = new NodeFila<T>();
-        atras = new NodeFila<T>();
-        tamanho = 0;
-    }
-    ~Fila() {
-        delete frente;
-        delete atras;
-    }
-    void enfileirar(T &item){
-        if (!empty()) {
-            NodeFila<T> *ultNode = atras->ant;
-            NodeFila<T> *newNode = new NodeFila<T>(item, NULL, ultNode);
-            ultNode->prox = newNode;
-            atras->ant = newNode;
-            tamanho++;
+	private:
+        int tamanho;
+        No<T> *frente, *atras;
+	public:
+        Fila(){
+            frente = new No<T>();
+            atras = new No<T>();
+            tamanho = 0;
         }
-        else {
-            NodeFila<T> *newNode = new NodeFila<T>(item);
-            frente->prox = newNode;
-            atras->ant = newNode;
-            tamanho++;
+        ~Fila() {
+                delete frente;
+                delete atras;
         }
-    }
-    T desenfileirar(){
-        if(!empty()){
-            NodeFila<T> *aux = frente;
-            frente = aux->getProx();
-            tamanho--;
-            T item = aux->getItem();
-            delete aux;
-            return item;
-        }else{
-            return T();
-        }
-
-    }
-    int getTam(){return tamanho;}
-    bool empty(){return tamanho == 0;}
-    NodeFila<T> * getFrente(){return this->frente->prox;}
-
-    NodeFila<T> extract_min(){
-        NodeFila<T> *it = frente->getProx();
-        NodeFila<T> *menor = it;
-        while(it!=NULL){
-            if (menor->getDist() > it->getDist()){
-                menor = it;
+        void enfileirar(T &item){
+            if (!empty()) {
+                No<T> *ultNo = atras->getAnt();
+                No<T> *no = new No<T>(item, ultNo);
+                ultNo->setProx(no);
+                atras->setAnt(ultNo->getProx());
+                tamanho++;
             }
-            it = it->getProx();
+            else {
+                No<T> *no = new No<T>(item);
+                atras->setAnt(no);
+                frente->setProx(atras->getAnt());
+                tamanho++;
+            }
         }
-
-        if(menor->ant!=NULL){
-            menor->ant->prox = menor->prox;
-        }else{
-            frente->prox = menor->prox;
+        T desenfileirar(){
+            if(!empty()){
+                No<T> *aux = frente->getProx();
+                frente->setProx(aux->getProx());
+                tamanho--;
+                T item = aux->getItem();
+                // delete aux;
+                return item;
+            }else{
+                return T();
+            }
         }
-        if (menor->prox!=NULL){
-            menor->prox->ant = menor->ant;
-        }else{
-            atras->ant = menor->ant;
-        }
-
-        NodeFila<T> retorno = *menor;
-        delete menor;
-        cout<<"Deleta: "<<tamanho<<endl;
-        tamanho--;
-        // cout<<"depois: "<<tamanho<<endl;
-        return retorno;
-    }
+        int getTam(){return tamanho;}
+        bool empty(){return tamanho == 0;}
+        No<T> * getFrente(){return frente;}
 };
 
+No<Vertice> extract_min(Fila<Vertice> *f){
+	return f->desenfileirar();
+}
 
-template <class T> class Graph
-{ 
-private:
-    int V;
-    T **adj;
-public:
-	Graph(){}
-    Graph(int V){
-        this->V = V;
-        adj = new T*[V+1];
-        for(int i = 0;i <= V; ++i)
-            adj[i] = new T[V+1];
-        for(int i = 0;i <= V; ++i)
-            for(int j = 0;j <= V; ++j)
-                adj[i][j] = T();
-    }
-    ~Graph(){    
-        for(int i = 0;i <= V; ++i)
-            delete[] adj[i];
-        delete[] adj; 
-    }
-	void createGraph(int V){
-        this->V = V;
-        adj = new T*[V+1];
-        for(int i = 0;i <= V; ++i)
-            adj[i] = new T[V+1];
-        for(int i = 0;i <= V; ++i)
-            for(int j = 0;j <= V; ++j)
-                adj[i][j] = T();
-    }
-    void addEdge(int v1, int v2, T w) {
-        adj[v1][v2] = w;
-        adj[v2][v1] = w;
-    }
-    void print() {
-        for(int i=1; i<=V; i++){
-            for(int j=1; j<=V; j++){
-                cout << fixed << setprecision(1) << adj[i][j] << "\t";
+class Grafo{
+    private:
+        int V;
+        float **adj;
+        Vertice *vertice;
+    public:
+        Grafo(){}
+        Grafo(int V){
+            this->V = V;
+            adj = new float*[V+1];
+            for(int i = 0;i <= V; ++i)
+                adj[i] = new float[V+1];
+            for(int i = 0;i <= V; ++i)
+                for(int j = 0;j <= V; ++j)
+                    adj[i][j] = float();
+            vertice = new Vertice[V+1];
+        }
+        ~Grafo(){
+            for(int i = 0;i <= V; ++i)
+                delete[] adj[i];
+            delete[] adj; 
+        }
+        void createGraph(int V){
+            this->V = V;
+            adj = new float*[V+1];
+            for(int i = 0;i <= V; ++i)
+                adj[i] = new float[V+1];
+            for(int i = 0;i <= V; ++i)
+                for(int j = 0;j <= V; ++j)
+                    adj[i][j] = float();
+            vertice = new Vertice[V+1];
+        }
+        void addEdge(int v1, int v2, float w) {
+            adj[v1][v2] = w;
+            adj[v2][v1] = w;
+        }
+        void print() {
+            for(int i=1; i<=V; i++){
+                for(int j=1; j<=V; j++){
+                    cout << fixed << setprecision(1) << adj[i][j] << "\t";
+                }
+                cout << endl;
             }
-            cout << endl;
         }
-    }
-    int getV(){ return V; }
-
-    void dijsktra (int s){
-        // INICIALIZA(G,s)
-        NodeFila<T> *vetorNode = new NodeFila<T>[getV()+1];
-        for (int i = 1; i <= getV(); i++){
-            vetorNode[i].dist = inf;
-            vetorNode[i].ant = NULL; 
-        }
-        vetorNode[s].dist = 0;
-
-        Fila<NodeFila<T>> S;
-        Fila<NodeFila<T>> Q;
-        for (int i = 1; i <= getV(); i++){
-            Q.enfileirar(vetorNode[i]);
-        }
-        NodeFila<NodeFila<T>> u;
-        while(!Q.empty()){
-            // EXTRACT-MIN(Q)
-            u = Q.extract_min();
-        }
-    }
+        int getV(){ return V; }
+        Vertice *getVertice(){return vertice;}
+        float getPeso(int v1, int v2){return adj[v1][v2];}
 };
+
+void INICIALIZA(Grafo *g, int s){
+	Vertice *v = g->getVertice();
+	for(int i = 1; i <= g->getV(); i++){
+        v[i].setId(i);
+		v[i].setDistancia(inf);
+		v[i].setAntecessor(NULL);
+	}
+	v[s].setDistancia(0);
+}
+
+void RELAXA(Vertice *u, Vertice *v, float w){
+	if (v->getDistancia() > u->getDistancia() + w) {
+		v->setDistancia(u->getDistancia() + w);
+		v->setAntecessor(u);
+	}
+}
+
+void DIJKSTRA(Grafo *g, int s){
+	INICIALIZA(g, s);
+	Fila<Vertice*> S;
+    Fila<Vertice*> Q;
+    Vertice *it = g->getVertice();
+    // Q = G.V
+    for(int i = 1; i <= g->getV(); i++){
+        Q.enfileirar(&it[i]);
+    }
+    Vertice u;
+    while(!Q.empty()){
+        u = Q.desenfileirar();
+        S.enfileirar(u);
+        for(int i = 1; i <= g->getV(); i++){
+            if(g->getPeso(u.getId(), i) > 0){
+                RELAXA()
+            }
+        }
+    }
+}
 
 int main(){
     int brain_n, brain_m, block_n, block_m, u, v, block_in, block_out, qtd_sick, sick;
     float weight;
     cin >> brain_n >> brain_m;
-    Graph<int> brain(brain_n), *block = new Graph<int>[brain_n+1];
+    Grafo<int> brain(brain_n), *block = new Grafo<int>[brain_n+1];
     for(int i = 1; i <= brain_m; i++){
         cin >> u >> v >> weight;
         brain.addEdge(u, v, weight);
